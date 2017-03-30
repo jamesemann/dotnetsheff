f97623f24222894fe87b2c1fef8cc9c1b7cf670d
James Mann <jamesemann158@gmail.com>
2017-03-30 16:29:01 +0100

    updated qna controller

--- qna-bot/QnaDemo/Dialogs/DemoQnaDialog.cs
+++ qna-bot/QnaDemo/Dialogs/DemoQnaDialog.cs
@@ -6,7 +6,7 @@ using QnAMakerDialog;
 namespace QnaDemo.Dialogs
 {
     [Serializable]
-    [QnAMakerService("646475201fe74e5aa1aed248c253d751", "3492b54e-5aaa-49b3-8fcd-2878782bd20d")]
+    [QnAMakerService("", "")]
     public class DemoQnaDialog : QnAMakerDialog<object>
     {
         public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
